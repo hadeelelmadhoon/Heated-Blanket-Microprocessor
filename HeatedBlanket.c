f13529c056b8c90c6277f6625b38f23188bba69f
hadeelelmadhoon <hadeelelmadhoon@users.noreply.github.com>
2020-03-23 15:06:26 -0400

    structure code

--- HeatedBlanket.c
+++ HeatedBlanket.c
@@ -4,3 +4,15 @@
 #define ADC_BASE 0xFF204000
 #define HEX3_HEX0_BASE 0xFF200020
 
+//declare and initialize pointers to the addresses
+volatile unsigned int * LED_ptr = (unsigned int *)LED_BASE;//Address of LED
+
+//functions
+
+
+
+int main(){
+    
+    
+}
+
