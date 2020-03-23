b4479e0d969859c67a19b1bc2ed3042663d00ff5
hashimsharkh <hashimabusharkh@gmail.com>
2020-03-23 15:42:52 -0400

    Update HeatedBlanket.c

--- HeatedBlanket.c
+++ HeatedBlanket.c
@@ -6,53 +6,22 @@
 #define BTN_BASE 0xFF200050
 
 
-//declare and initialize pointers to the addresses
-
-volatile unsigned int * LED_ptr = (unsigned int *)LED_BASE;//Address of LED
-volatile unsigned int * HEX_ptr = (unsigned int *)HEX3_HEX0_BASE;
-volatile unsigned int * BTN_ptr = (unsigned int *)BTN_BASE;
 
 //declare gloabl variables
 
 volatile float desiredTemp;
 volatile float currTemp;
 
-int lookUpTable[10] = 
-	{0x3F, 0x6, 0x5B, 0x4F, 0x66, 
-	0x6D, 0x7D, 0x7, 0x7F, 0x67};
-	
-
-
-
-
-*HEX_ptr = lookUpTable[0];
-*HEX_ptr |= lookUpTable[0] << 16;
-*HEX_ptr |= lookUpTable[0] << 24;
-
-//functions
-
-void decreaseTemp() {
-    currTemp -= 1;
-}
 
 void increaseTemp() {
-    currTemp += 1;
+	currTemp += 1;
 }
 
-float HexToDecimal(){
-
-	unsigned int decimalValue = *HEX_ptr & 0xff;
-	unsigned int unitsValue = *HEX_ptr & 0xff0000;
-	unsigned int tensValue = *HEX_ptr & 0xff000000;
-
-}
-
-void stimulateHeat(){
-    float currentTemp = hexToDecimal();
+void stimulateHeat(unsigned int *LED_ptr){
     unsigned int LED_value = 0x0;
     
     //turn on LED
-    if(currentTemp <= desiredTemp - 2.0){
+    if(curr <= desiredTemp - 2.0){
         LED_value = 0x1;
         *LED_ptr = LED_value;
         increaseTemp();
@@ -63,9 +32,42 @@ void stimulateHeat(){
     }
 }
 
+//functions
+
+void decreaseTemp() {
+	currTemp -= 1;
+}
+
+
+float HexToDecimal() {
+
+	unsigned int decimalValue = *HEX_ptr & 0xff;
+	unsigned int unitsValue = *HEX_ptr & 0xff0000;
+	unsigned int tensValue = *HEX_ptr & 0xff000000;
+
+}
+
 int main(){
+	//declare and initialize pointers to the addresses
+
+	volatile unsigned int * LED_ptr = (unsigned int *)LED_BASE;//Address of LED
+	volatile unsigned int * HEX_ptr = (unsigned int *)HEX3_HEX0_BASE;
+	volatile unsigned int * BTN_ptr = (unsigned int *)BTN_BASE;
+
     //turn off all LEDs to start
     unsigned int LED_value = 0x0;
     *LED_ptr = LED_value;
+
+	int lookUpTable[10] =
+	{ 0x3F, 0x6, 0x5B, 0x4F, 0x66,
+	0x6D, 0x7D, 0x7, 0x7F, 0x67 };
+
+
+	*HEX_ptr = lookUpTable[0];
+	*HEX_ptr |= lookUpTable[0] << 16;
+	*HEX_ptr |= lookUpTable[0] << 24;
+
+
+
     
 }
